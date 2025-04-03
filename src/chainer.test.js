8fa220c2ded55ef0c9756971b32f0fff593c9a86
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-04-03 08:13:26 +0200

    unary-function-chainer

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,3 +1,37 @@
-'use strict'
+'use strict';
 
-const { chainer } = require('./chainer');
+describe('chainer', () => {
+  const { chainer } = require('./chainer');
+
+  function f1(x) {
+    return x * 2;
+  }
+
+  function f2(x) {
+    return x + 2;
+  }
+
+  function f3(x) {
+    return Math.pow(x, 2);
+  }
+
+  it('should return a function', () => {
+    expect(typeof chainer([f1, f2, f3])).toBe('function');
+  });
+
+  it('should apply functions in order', () => {
+    expect(chainer([f1, f2, f3])(0)).toBe(4);
+  });
+
+  it('should work with a single function', () => {
+    expect(chainer([f1])(5)).toBe(10);
+  });
+
+  it('should work with an empty function list', () => {
+    expect(chainer([])(10)).toBe(10);
+  });
+
+  it('should correctly apply multiple functions', () => {
+    expect(chainer([f1, f2, f3])(2)).toBe(36); // (2 * 2) + 2 = 6, 6^2 = 36
+  });
+});
